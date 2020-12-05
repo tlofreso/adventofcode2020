f45576dcf90f624bbf9a4cd6214ee789bf6742b5
Tony Lofreso Jr <tonylofresojr@gmail.com>
2020-12-04 20:31:08 -0500

    still working on solving

--- 04/passport.py
+++ 04/passport.py
@@ -25,17 +25,71 @@ def regexer():
 
     data = values()
 
-    matcher = re.compile(r"(?P<birth_year>byr:\d\d\d\d)")
-    test = matcher.match(data[0])
-    print(test)
+    byr = re.compile(r"(?P<birth_year>byr:\d{4})")
+    iyr = re.compile(r"(?P<issue_year>iyr:\d{4})")
+    eyr = re.compile(r"(?P<expiration_year>eyr:\d{4})")
+    hgt = re.compile(r"(?P<height>hgt\:\d+\w+)")
+    hcl = re.compile(r"(?P<hair_color>hcl\:\S+)")
+    ecl = re.compile(r"(?P<eye_color>ecl\:\S+)")
+    pid = re.compile(r"(?P<passport_id>pid\:\S+)")
+    cid = re.compile(r"(?P<country_id>cid\:\S+)")
 
-    print(data[0])
-    for line in data[0].splitlines():
-        m = matcher.match(line)
-        # m = re.match(r"(?P<birth_year>byr:\d\d\d\d)", line)
-        print(m)
-        # print(m.group("birth_year"))
+    test = []
+    for line in data:
+
+        if byr.match(line) is not None:
+            birth_yr = byr.match(line)
+        else:
+            birth_yr = None
+
+        if iyr.match(line) is not None:
+            issue_yr = iyr.match(line)
+        else:
+            issue_yr = None
+
+        if eyr.match(line) is not None:
+            expire_yr = eyr.match(line)
+        else:
+            expire_yr = None
+
+        if hgt.match(line) is not None:
+            height = hgt.match(line)
+        else:
+            height = None
+
+        if hcl.match(line) is not None:
+            hair_color = hcl.match(line)
+        else:
+            hair_color = None
+
+        if ecl.match(line) is not None:
+            eye_color = ecl.match(line)
+        else:
+            eye_color = None
+
+        if pid.match(line) is not None:
+            passport_id = pid.match(line)
+        else:
+            passport_id = None
+
+        if cid.match(line) is not None:
+            country_id = cid.match(line)
+        else:
+            country_id = None
+
+        test.append(
+            birth_yr.group("birth_year"),
+            issue_yr.group("issue_year"),
+            expire_yr.group("expiration_year"),
+            height.group("height"),
+            hair_color.group("hair_color"),
+            eye_color.group("eye_color"),
+            passport_id.group("passport_id"),
+            country_id.group("country_id"),
+        )
+
+    return test
 
 
 if __name__ == "__main__":
-    regexer()
+    print(regexer())
