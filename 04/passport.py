3b05d4890844c9fc24cabd3eb4e9bfc4dea35de1
Tony Lofreso Jr <tonylofresojr@gmail.com>
2020-12-05 11:37:34 -0500

    finished puzzle 1

--- 04/passport.py
+++ 04/passport.py
@@ -17,79 +17,65 @@ def values():
 
     with open("input.txt") as f:
         my_input = f.read().split(2 * os.linesep)
+        puzzle_input = list(map(lambda x: x.replace("\n", " "), my_input))
 
-    return my_input
+    return puzzle_input
 
 
 def regexer():
 
+    # byr = re.compile(r"(?P<birth_year>byr:\d{4})")
+    # iyr = re.compile(r"(?P<issue_year>iyr:\d{4})")
+    # eyr = re.compile(r"(?P<expiration_year>eyr:\d{4})")
+    # hgt = re.compile(r"(?P<height>hgt\:\d+\w+)")
+    # hcl = re.compile(r"(?P<hair_color>hcl\:\S+)")
+    # ecl = re.compile(r"(?P<eye_color>ecl\:\S+)")
+    # pid = re.compile(r"(?P<passport_id>pid\:\S+)")
+    # cid = re.compile(r"(?P<country_id>cid\:\S+)")
+
+    r = re.compile(
+        r"byr:\d{4}|iyr:\d{4}|eyr:\d{4}|hgt\:\d+\w+|hcl\:\S+|ecl\:\S+|pid\:\S+|cid\:\S+"
+    )
+
     data = values()
 
-    byr = re.compile(r"(?P<birth_year>byr:\d{4})")
-    iyr = re.compile(r"(?P<issue_year>iyr:\d{4})")
-    eyr = re.compile(r"(?P<expiration_year>eyr:\d{4})")
-    hgt = re.compile(r"(?P<height>hgt\:\d+\w+)")
-    hcl = re.compile(r"(?P<hair_color>hcl\:\S+)")
-    ecl = re.compile(r"(?P<eye_color>ecl\:\S+)")
-    pid = re.compile(r"(?P<passport_id>pid\:\S+)")
-    cid = re.compile(r"(?P<country_id>cid\:\S+)")
+    passports = []
 
-    test = []
-    for line in data:
+    for e in data:
+        parsed = r.findall(e)
+        passports.append(parsed)
 
-        if byr.match(line) is not None:
-            birth_yr = byr.match(line)
-        else:
-            birth_yr = None
+    return passports
 
-        if iyr.match(line) is not None:
-            issue_yr = iyr.match(line)
-        else:
-            issue_yr = None
 
-        if eyr.match(line) is not None:
-            expire_yr = eyr.match(line)
-        else:
-            expire_yr = None
+def puzzle1():
 
-        if hgt.match(line) is not None:
-            height = hgt.match(line)
-        else:
-            height = None
+    required = ("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
+    passports = regexer()
 
-        if hcl.match(line) is not None:
-            hair_color = hcl.match(line)
-        else:
-            hair_color = None
+    counter = 0
 
-        if ecl.match(line) is not None:
-            eye_color = ecl.match(line)
-        else:
-            eye_color = None
+    for passport in passports:
+        for e in passport:
+            if e.startswith("cid"):
+                passport.remove(e)
 
-        if pid.match(line) is not None:
-            passport_id = pid.match(line)
-        else:
-            passport_id = None
+    for passport in passports:
+        valid = []
+        for e in passport:
+            entry = e.startswith(required)
+            valid.append(entry)
 
-        if cid.match(line) is not None:
-            country_id = cid.match(line)
-        else:
-            country_id = None
+        if False not in valid:
+            if len(valid) == 7:
+                counter += 1
 
-        test.append(
-            birth_yr.group("birth_year"),
-            issue_yr.group("issue_year"),
-            expire_yr.group("expiration_year"),
-            height.group("height"),
-            hair_color.group("hair_color"),
-            eye_color.group("eye_color"),
-            passport_id.group("passport_id"),
-            country_id.group("country_id"),
-        )
-
-    return test
+    return counter
 
 
 if __name__ == "__main__":
-    print(regexer())
+    print(puzzle1())
+
+
+# if all([x.startswith(y) for y in required]):
+#                 print(x)
