db5e4a8f3a3af111f0e926152ed9798706705278
Tony Lofreso Jr <tonylofresojr@gmail.com>
2020-12-06 20:46:21 -0500

    Advent of Code - Day 6

--- 06/customs.py
+++ 06/customs.py
@@ -6,20 +6,33 @@ def values():
 
     with open("input.txt") as f:
         my_input = f.read().split(2 * os.linesep)
-        my_input = list(map(lambda x: x.replace("\n", ""), my_input))
+        puzzle1_input = list(map(lambda x: x.replace("\n", ""), my_input))
 
-    return my_input
+    return puzzle1_input, my_input
 
 
 def puzzle1():
 
     yeses = []
-    for group in values():
+    for group in values()[0]:
         yes = len(set(group))
         yeses.append(yes)
 
     return yeses
 
 
+def puzzle2():
+
+    yeses = []
+    for group in values()[1]:
+        group = group.splitlines()
+        yes = len(set(group[0]).intersection(*group))
+        yeses.append(yes)
+
+    return yeses
+
+
 if __name__ == "__main__":
+
     print(sum(puzzle1()))
+    print(sum(puzzle2()))
